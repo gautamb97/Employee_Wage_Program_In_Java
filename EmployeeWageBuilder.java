cbbe03439912000743a6f64fdbe1ebc31c36656f
icode32 <www.gautam971997@gmail.com>
2020-11-10 20:24:30 +0530

    Added:Checking_Employee_Presence

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,7 +4,14 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		//WELCOME TO EMPLOYEE WAGE COMPUTATION
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTAION");
-
+		//Constants
+		int IS_FULL_TIME = 1;
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
