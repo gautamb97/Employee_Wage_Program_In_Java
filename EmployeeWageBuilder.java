783b918341f04c4b9208392a490e8660341d0f05
icode32 <www.gautam971997@gmail.com>
2020-11-10 22:35:53 +0530

    Modified:Calculated_Wages_For_A_Month

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,14 +3,15 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
 	
 	public static void main(String[] args) {
 		//WELCOME TO EMPLOYEE WAGE COMPUTATION
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTAION");
 		//Variables
-		int empHrs = 0;
-		int empWage = 0;
+		int empHrs = 0, empWage = 0, totalEmpWage = 0;
 		//Computation
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 		case IS_PART_TIME:
@@ -23,7 +24,10 @@ public class EmployeeWageBuilder {
 			empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
 		System.out.println("Emp Wage: " + empWage);
+		}
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 
 }
