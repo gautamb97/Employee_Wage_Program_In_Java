4c0d7de8b8ad83fb712fc35cc62c550f34d5228f
icode32 <www.gautam971997@gmail.com>
2020-11-10 20:00:33 +0530

    Initial_Commit

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,10 @@
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		//WELCOME TO EMPLOYEE WAGE COMPUTATION
+		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTAION");
+
+	}
+
+}
