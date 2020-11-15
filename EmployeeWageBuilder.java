93a744b4482ebd07eb8fb3c17f1d6ccb4aab7ae5
icode32 <www.gautam971997@gmail.com>
2020-11-15 21:18:44 +0530

    Modified:Ability_To_Save_The_Total_Wage_For_Each_Company

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,7 +3,19 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+	
+	public EmployeeWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+	public void computeEmpWage() {
 		//Variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		//Computation
@@ -23,14 +35,20 @@ public class EmployeeWageBuilder {
 		totalEmpHrs += empHrs;
 		System.out.println("Days#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage for Company: " +company+" is:"+totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+	}
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company: " +company+ "is:" +totalEmpWage;
 	}
 	public static void main(String[] args) {
 		//WELCOME TO EMPLOYEE WAGE COMPUTATION
 			System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTAION");
-		computeEmpWage("DMart", 20, 2, 10);
-		computeEmpWage("Reliance", 10, 4, 20);
+		EmployeeWageBuilder dMart = new EmployeeWageBuilder("DMart", 20, 2, 10);
+		EmployeeWageBuilder reliance = new EmployeeWageBuilder("Reliance", 10, 4, 20);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
 }
